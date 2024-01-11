c1e967f1c6fe379d2ac4ae10e62114c1432908cc
Mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-01-11 12:35:40 -0700

    BIG change.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//Some basic scripts made from Js to work the website, and make it interactive.
+
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+
 function whatsNew() {
 document.getElementById("boxChanger").innerHTML = '<h2>Whats New?</h2><p>The bark explore page is being created.</p>';
 }
@@ -9,9 +16,9 @@ function recentCom() {
 function randomTxt() {
 //geeksforgeeks.org for the random text generator.
 var wrd;
-let arr = ["Bark! woof!", "Bark is a mod of scratch with extensions",
-    "Scratch dog 🐶", "Bark is being created, so stay patient!!", "note: Bark does not support accounts yet", 
-"funfact: Dogs have insane hearing and smelling.", "We need a scratch dog!"];
+let arr = ["Bark! woof!", "Bark is a mod of scratch with new extensions",
+    "Scratch dog 🐶", "Bark is being created, so stay patient!!", "Note: Bark does not support accounts yet", 
+"Funfact: Dogs have insane hearing and smelling.", "We need a scratch dog!", "Who not create a project??", "A special version is here somewere...🐕"];
 
 wrd = (arr[(Math.floor(Math.random() * arr.length))]);
 document.getElementById("randomWord").innerHTML = wrd;
