cef18e9e549d49473f671414db3590c3c3c76630
Mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-01-16 11:24:13 -0700

    Changing "images/picture" to "Images/picture".

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ function randomTxt() {
     var wrd;
     let arr = ["Bark! woof!", "Bark is a mod of scratch with new extensions",
     "Scratch dog 🐶", "Bark is being created, so stay patient!!", "Note: Bark does not support accounts yet", 
-    "Funfact: Dogs have insane hearing and smelling.", "We need a scratch dog!", "Who not create a project??", "A special version is here somewere...🐕"];
+    "Funfact: Dogs have insane hearing and smelling.", "We need a scratch dog!", "Who not create a project??", "A special version is here somewere..",
+    "Bark is hosted with github."];
 
     wrd = (arr[(Math.floor(Math.random() * arr.length))]);
     document.getElementById("randomWord").innerHTML = wrd;
