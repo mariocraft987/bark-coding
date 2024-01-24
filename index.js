696cbf8781ad8489ffa82d087b5cfe4bd0c6d758
Mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-01-23 20:22:57 -0700

    No more blank forms, Haha!

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function randomTxt() {
     let arr = ["Bark! woof!", "Bark is a mod of scratch with new extensions",
     "Scratch dog", "Bark is being created, so stay patient!!", "Note: Bark does not support accounts yet", 
     "Funfact: Dogs have insane hearing and smelling.", "We need a scratch dog!", "Why not help with bark??", "A special version is here somewere..",
-    "Bark is hosted with github.", "Funfact: im a funfact!", "Bark is made from penguin mod made from turbowarp made from scratch.", 
+    "Bark is hosted with github pages.", "Funfact: im a funfact!", "Bark is made from penguin mod made from turbowarp etc.", 
     "Wanna help create bark? go <a href='Jobs.html' class='link'>Here</a>"];
 
     wrd = (arr[(Math.floor(Math.random() * arr.length))]);
@@ -28,7 +28,9 @@ function randomTxt() {
 
 function checkSignup() {
 let username = document.getElementById('username').value;
-document.getElementById('page').innerHTML = '<br/><h2>Welcome to Bark ' + username + '!</h2>';
+if (username != '') {
+            document.getElementById('page').innerHTML = '<br/><h2>Welcome to Bark ' + username + '!</h2>';
+    }
 }
 
 function jobRegister() {
