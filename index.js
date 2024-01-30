5e004d8d5a130434d4f6feda96238355069efd06
Mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-01-29 18:24:41 -0700

    New splash text!

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ function recentCom() {
 function randomTxt() {
     //geeksforgeeks.org for the random text generator.
     var wrd;
-    let arr = ["Bark! woof!", "Bark is a mod of scratch with new extensions",
+    let arr = ["Bark! woof!", "Bark is a mod of scratch with new extensions", 
     "Scratch dog", "Bark is being created, so stay patient!!", "Note: Bark does not support accounts yet", 
-    "Funfact: Dogs have insane hearing and smelling.", "We need a scratch dog!", "Why not help with bark??", "A special version is here somewere..",
-    "Bark is hosted with github pages.", "Funfact: im a funfact!", "Bark is made from penguin mod made from turbowarp etc.", 
-    "Wanna help create bark? go <a href='Jobs.html' class='link'>Here</a>", "Bark's birthday month is October"];
+    "Funfact: Dogs have insane hearing and smelling.", "We need a scratch dog!", "Why not help with bark??", "A special version is here somewere..", 
+    "Bark is hosted with github pages.", "Funfact: im a funfact!", "Bark is made from penguin mod made from turbowarp etc.", "Whats cooler, Dogs or Cats?", 
+    "Wanna help create bark? go <a href='Jobs.html' class='link'>Here</a>", "Bark's birthday month is October", 
+    "Why not go <a href='https://www.scratchfoundation.org/donate' class='link'>donate</a> to scratch?", "Go check out snail-ide!", ":amongus:", 
+    "This text has a 0.07 chance of apearing", "So <em>many</em> breeds of dogs", "Check out our <a href='https://github.com/mariocraft987/bark.github.io/' class='link'>github</a>"];
 
     wrd = (arr[(Math.floor(Math.random() * arr.length))]);
     document.getElementById("randomWord").innerHTML = wrd;
