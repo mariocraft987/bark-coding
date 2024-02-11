97d083c918c2bee8a49b80b5b54e7df6af089777
Mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-02-10 19:20:47 -0700

    title="somthing" for commits page

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function recentCom() {
         .then(commits => {
             var Commits = '<h2>Recent Commits</h2><ul>';
             commits.slice(0, 4).forEach(commit => {
-                Commits += `<li class="none-bullets"><sup><a href="https://github.com/${commit.author.login}"><img style="border-radius:25px;margin-bottom:-10px;" src="https://github.com/${commit.author.login}.png" width="21"></a></sup><a href="${commit.html_url}" class="link">${commit.commit.message}</a></li>`;
+                Commits += `<div title="${commit.author.login}: ${commit.commit.message}"><li class="none-bullets"><a href="https://github.com/${commit.author.login}"><img style="border-radius:25px;margin-bottom:-3px;" src="https://github.com/${commit.author.login}.png" width="21"></a><a href="${commit.html_url}" class="link">${commit.commit.message}</a></li></div>`;
             });
             Commits += '</ul>';
             document.getElementById("boxChanger").innerHTML = censor(Commits + "<br/>");
