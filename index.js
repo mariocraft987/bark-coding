188f15fae263508972ee37b60b919a243f05a17c
Mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-02-10 19:46:23 -0700

    dark and light mode

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 /* Some basic scripts created in javascript to work the website, and make it interactive. */
 console.log("Exit Now! This part is used for devolopers.");
 
+    content = '<div class="navbar"><a href="home.html"><img src="src/images/Logo.svg" alt="Bark" width="25" height="25" ></a><a href="editor.html">Create</a><a href="explore.html">Explore</a>'
+    content += '<a href="ideas.html">ideas</a><a href="about.html">About</a><a href="login.html" class="right">Sign in</a><a href="signup.html" class="right">Join bark</a>'
+    content += '<button class="dark-mode-button" onclick="toggleDarkMode()"><img src="https://editor.snail-ide.com/static/assets/90e939d170002c61a50116ed55763b19.svg" alt="Dark Mode"></button></div>'
+    document.getElementById('navbar').innerHTML = content;
+
 function whatsNew() {
     var News;
     News = '<h2>Whats New?</h2><p>The Bark Editor has a very early alpha! It is being made with Blockly!</p>';
@@ -14,7 +19,7 @@ function recentCom() {
         .then(commits => {
             var Commits = '<h2>Recent Commits</h2><ul>';
             commits.slice(0, 4).forEach(commit => {
-                                Commits += `<div title="${commit.author.login}: ${commit.commit.message}"><li class="none-bullets"><a href="https://github.com/${commit.author.login}"><img style="border-radius:25px;margin-bottom:-3px;" src="https://github.com/${commit.author.login}.png" width="21"></a><a href="${commit.html_url}" class="link">${commit.commit.message}</a></li></div>`;
+                Commits += `<div title="${commit.author.login}: ${commit.commit.message}"><li class="none-bullets"><a href="https://github.com/${commit.author.login}"><img style="border-radius:25px;margin-bottom:-3px;" src="https://github.com/${commit.author.login}.png" width="21"></a><a href="${commit.html_url}" class="link">${commit.commit.message}</a></li></div>`;
             });
             Commits += '</ul>';
             document.getElementById("boxChanger").innerHTML = censor(Commits + "<br/>");
@@ -58,3 +63,32 @@ function jobRegister() {
     document.getElementById('page').innerHTML = '<br/><h2>You have selected a Job to ' + job + '</h2><p>We have to run a few tests to see your actually <em>'+ username +'</em></p><br/><br/><p>Please comment "Job Register 4543"';
     document.getElementById('page').innerHTML += ' on <a class="link" href="https://github.com/Mariocraft987/bark.github.io/commit/5644df6ebc5aa7ea7b611141a265ff7c17712c5e">this commit</a>.</p>'
     }
+
+    /* @mnsderp for the data saving scripts */
+
+    // Function to toggle dark mode
+  function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    // Save dark mode state to local storage
+    const isDarkMode = body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode);
+  }
+
+  // Function to initialize dark mode based on local storage
+  function initializeDarkMode() {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    const body = document.body;
+    if (isDarkMode) {
+      body.classList.add('dark-mode');
+    } else {
+      body.classList.remove('dark-mode');
+    }
+  }
+
+  // Attach event listener to dark mode toggle button
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  darkModeToggle.addEventListener('click', toggleDarkMode);
+
+  // Initialize dark mode when the page loads
+  window.addEventListener('load', initializeDarkMode);
