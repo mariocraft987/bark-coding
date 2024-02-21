2a4a50c1cb4d2b80c2d59fbdd82e1744545c5f7a
qwertyy__ <129226914+qwertyytheartist@users.noreply.github.com>
2024-02-20 22:40:28 -0800

    link class

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function recentCom() {
         .then(commits => {
             var Commits = '<h2>Recent Commits</h2><ul>';
             commits.slice(0, 4).forEach(commit => {
-                Commits += `<div title="${commit.author.login}: ${commit.commit.message}"><li><a href="https://github.com/${commit.author.login}"><img style="border-radius:25px;margin-bottom:-3px;" src="https://github.com/${commit.author.login}.png" width="21"></a><a href="${commit.html_url}">${commit.commit.message}</a></li></div>`;
+                Commits += `<div title="${commit.author.login}: ${commit.commit.message}"><li><a href="https://github.com/${commit.author.login}"><img style="border-radius:25px;margin-bottom:-3px;" src="https://github.com/${commit.author.login}.png" width="21"></a><a href="${commit.html_url}" class="link">${commit.commit.message}</a></li></div>`;
             });
             Commits += '</ul>';
             document.getElementById("boxChanger").innerHTML = censor(Commits + "<br/>");
