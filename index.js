b9d7718f42cd4eca3dede0b5103ecc662bf589e6
mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-02-21 13:20:27 -0700

    THIS BETTER WORK

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /* Some basic scripts created in javascript to work the website, and make it interactive. */
 console.log("Exit Now! This part is used for devolopers.");
 
-    content = '<div class="navbar"><a href="home.html"><img src="src/images/Logo.svg" alt="Bark" width="25" height="25" ></a><a href="editor.html">create</a><a href="explore.html">explore</a>'
-    content += '<a href="ideas.html">ideas</a><a href="about.html">about</a><a href="https://barkprogram.boards.net">discuss</a><a href="login.html" class="right">sign in</a><a href="signup.html" class="right">join bark</a>'
-    content += '<a class="dark-mode-button" id="darkModeToggle"><dmbico alt="Dark Mode"></dmbico></a></div>'
+content = '<div class="navbar"><a href="home.html"><img src="src/images/Logo.svg" alt="Bark" width="25" height="25" ></a><a href="editor.html">create</a><a href="explore.html">explore</a>'
+content += '<a href="ideas.html">ideas</a><a href="about.html">about</a><a href="#">discuss</a><a href="login.html" class="right">sign in</a><a href="signup.html" class="right">join bark</a>'
+content += '<button class="dark-mode-button" id="darkModeToggle"><img src="https://editor.snail-ide.com/static/assets/90e939d170002c61a50116ed55763b19.svg" alt="Dark Mode"></button></div>'
     document.getElementById('navbar').innerHTML = content;
 
 function whatsNew() {
