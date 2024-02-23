02e4c9bbaefc8f585de64e23084cf4da5b1b2a65
Rubiidev / Circle IDE Owner <158863401+Rubiidev-18@users.noreply.github.com>
2024-02-23 15:56:59 -0700

    many commits

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function recentCom() {
         .then(response => response.json())
         .then(commits => {
             var Commits = '<h2>Recent Commits</h2><ul>';
-            commits.slice(0, 5).forEach(commit => {
+            commits.slice(0, 9).forEach(commit => {
                 Commits += `<div title="${commit.author.login}: ${commit.commit.message}"><li><a href="https://github.com/${commit.author.login}"><img style="border-radius:12px;margin-top:-4px;margin-left:-6px" src="https://github.com/${commit.author.login}.png" width="21"></a><a href="${commit.html_url}">${commit.commit.message}</a></li></div>`;
             });
             Commits += '</ul>';
