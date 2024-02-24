79cb04e7969a866df01be292978c038b751ab656
mariocraft <154646419+Mariocraft987@users.noreply.github.com>
2024-02-23 17:33:27 -0700

    AUUGGH

--- src/scripts/censoring.js
+++ src/scripts/censoring.js
@@ -11,9 +11,19 @@ function t(length) {
 
 function censor(text) {
     return String(text)
-        .replaceAll(/(shitting)+/gim, t(8))
-        .replaceAll(/(fucking|shut up)+/gim, t(7))
-        .replaceAll(/(bitch|pussy)+/gim, t(5))
-        .replaceAll(/(fuck|shit|fock)+/gim, t(4))
-        .replaceAll(/(ass|sex|fok)+/gim, t(3))
+        // 🔞 i highly reccomend not searching up some of the stuff on here 🔞
+        .replaceAll(/(double penetration)+/gim, t(18))
+        .replaceAll(/(pearl necklace|rusty trombone)+/gim, t(14))
+        .replaceAll(/(sex position|mating press|white power|sex offender)+/gim, t(12))
+        .replaceAll(/(doggy style|intercourse)+/gim, t(11))
+        .replaceAll(/(kitten lip|strip club\pornograph\paedophile)+/gim, t(10))
+        .replaceAll(/(kitty lip|anilingus|camel toe|masturbat|wet dream|stripper|pedophile|marijuana)+/gim, t(9))
+        .replaceAll(/(dick pic|ball sac|pleasure|oral sex|anal sex|creampie|cum shot|fellatio|she-male|she male|tamakeri|cameltoe|ejaculat|find you|jerk off|assmunch|babeland|god damn)+/gim, t(8))
+        .replaceAll(/(shut up|shaddup|goddamn|god dam|the hub|pornhub|tea bag|scrotum|ballsac|bukkake|facesit|fluffer|shemale|upskirt|mammary|quickie|undress|gay sex|erotism|eat ass|asshole|bastard|cumming)+/gim, t(7))
+        .replaceAll(/(penile|vagina|nipple|teabag|ekiben|erotic|tranny|sexcam|rimjob|raping|rapist|rectum|pantie|orgasm|nudity|faggot|incest|murder|hentai|retard|my ass|breast)+/gim, t(6))
+        .replaceAll(/(bitch|pussy|whore|f off|effin|penis|erect|semen|lmfao|smash|cheek|seggs|teste|prick|squaw|thong|vulva|naked|horny|dildo|booty|boner|balls)+/gim, t(5))
+        .replaceAll(/(fuck|shit|fock|cunt|cock|dick|gyat|boob|butt|lmao|nude|porn|wank|slut|milf|dilf|yiff|yaoi|twat|tush|rape|pedo|nsfw|jizz|kike|jugg|kill|hump|tidd|2g1c|anal|anus)+/gim, t(4))
+        .replaceAll(/(p ⭐️|⬛️ 🟧|cbt|bbc|kys|fag|tit|cum)+/gim, t(3))
+        .replaceAll(/(p⭐️|⬛️🟧|69)+/gim, t(2))
+        .replaceAll(/(🤱|💦|🕳️|🤤|🍑|🥒|🍆|🖕|😩|😫|🥴|🚛|🍒|🍝|🌽|👅|🥵)+/gim, t(1))
 }
