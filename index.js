5e4b4edf30804032069b9750ef4e1e06759148bf
mariocraft <154646419+Mariocraft987@users.noreply.github.com>
2024-02-25 14:39:29 -0700

    frig!!

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function recentCom() {
         .then(commits => {
             var Commits = '<h2>Recent Commits</h2><ul style="overflow-y: scroll; height: 150px;"><br/>';
             commits.slice(0, 14).forEach(commit => {
-                Commits += `<div title="${censor(commit.author.login)}: ${censor(commit.commit.message)}"><li><a href="https://github.com/${censor(commit.author.login)}"><img style="border-radius:12px;margin-top:-4px;margin-left:-6px" src="https://github.com/${commit.author.login}.png" width="21"></a><a href="${commit.html_url}">${censor(commit.commit.message)}</a></li></div>`;
+                Commits += `<div title="${replace(commit.author.login)}: ${replace(commit.commit.message)}"><li><a href="https://github.com/${replace(commit.author.login)}"><img style="border-radius:12px;margin-top:-4px;margin-left:-6px" src="https://github.com/${commit.author.login}.png" width="21"></a><a href="${commit.html_url}">${censor(commit.commit.message)}</a></li></div>`;
             });
             Commits += '</ul><br><a class="buttonFrBx" href="https://github.com/Mariocraft987/bark.github.io/commits/main/" style="text-align: center;">See all</a><br>';
             finalCommits = Commits.replaceAll(":trollface:", "<img src='src/emojis/svg/troll.svg' width='23'>")
