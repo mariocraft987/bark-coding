e06b6e52656c8d7e175e58c26518623ce422868e
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-02-26 11:06:45 -0700

    dis will probably work

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function recentCom() {
         .then(response => response.json())
         .then(commits => {
             var Commits = '<h2>Recent Commits</h2><ul style="overflow-y: scroll; height: 150px;"><br/>';
-            commits.slice(0, 14).forEach(commit => {
+            commits.slice(0, 22).forEach(commit => {
                 Commits += `<div title="${replace(commit.author.login)}: ${replace(commit.commit.message)}"><li><a href="https://github.com/${replace(commit.author.login)}"><img style="border-radius:12px;margin-top:-4px;margin-left:-6px" src="https://github.com/${commit.author.login}.png" width="21"></a><a href="${commit.html_url}">${replace(commit.commit.message)}</a></li></div>`;
             });
             Commits += '</ul><br><a class="buttonFrBx" href="https://github.com/Mariocraft987/bark.github.io/commits/main/" style="text-align: center;">See all</a><br>';
