d1a7c6821dcb167a926ccf844ec278e8295908a3
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-02-29 17:22:22 -0700

    1 little "."

--- src/scripts/replacer.js
+++ src/scripts/replacer.js
@@ -71,7 +71,7 @@ function replace(text) {
         .replaceAll(":dumo:", "<img src='src/emojis/svg/dev/dumo.svg' alt='dev' style='margin-bottom: -7px;' width='"+ emojiWidth + "'>")
 
         // joke emojis
-        replaceAll(":mewing:", "<img src='src/emojis/svg/joke/mewing.svg' alt='mewing' style='margin-bottom: -7px;' width='"+ emojiWidth + "'>")
+        .replaceAll(":mewing:", "<img src='src/emojis/svg/joke/mewing.svg' alt='mewing' style='margin-bottom: -7px;' width='"+ emojiWidth + "'>")
 
         // devoloper tags
         .replaceAll(/(@mariocraft987|@mariocraft )+/gim, '<a href="https://github.com/Mariocraft987" style="background-color: #988bbf;height: 16px;display: inline-block;padding: 5px;margin: -5px 3px;border: 1px solid #ebd7ff;border-radius: 10px;color: #ffffff;position: relative;top: 2px; text-decoration: none;"><img src="https://github.com/mariocraft987.png" width="16px" style="border-radius: 50%;"><span style="top: -2px;margin-left: 3px;margin-right: 3px;position: relative;">mariocraft</span></a>')
