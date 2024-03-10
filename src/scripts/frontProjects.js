ced4687ad8e5a2179c28972f5156a5043c7bb6a1
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-03-09 17:43:53 -0700

    Create frontProjects.js

---
+++ src/scripts/frontProjects.js
@@ -0,0 +1,8 @@
+function featuredProjects() {
+  let featured = {};
+
+  // List of featured projects
+  featured["100000"] = ["Dummy Project"];
+
+  document.getElementById('frontPage').innerHTML = "";
+}
