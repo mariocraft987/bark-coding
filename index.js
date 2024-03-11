4604747acf1ef141bce2e6acea08c69b10d4aded
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-03-10 17:43:53 -0700

    Imma take a break

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log("Exit Now! This part is used for devolopers.");
 // navbar
 content = '<div class="navbar"><a href="home.html"><img src="src/images/Logo.svg" alt="bark" width="25" height="25" ></a><a href="editor_new">create</a><a href="explore.html">explore</a>'
 content += '<a href="ideas.html">ideas</a><a href="about.html">about</a><a href="https://barkprogram.boards.net/">discuss</a>'
-    if (localStorage.getItem("myBarkUsername") != "") {
+    if (localStorage.getItem("myBarkUsername") == "") {
         content += '<a class="right">sign out</a><a class="right">profile</a>'
     }else{
         content += '<a href="login.html" class="right">sign in</a><a href="signup.html" class="right">join bark</a>'
