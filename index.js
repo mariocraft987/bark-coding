964b1aa8758b3f19e24d7847081bd7883e500efd
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-03-16 21:14:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Some basic scripts created in javascript to work the website, and make it interactive. */
 
-let newsBox = "<a href='https://github.com/nmsderp'>nmsderp!</a> has added functionality to the move/move back blocks, these blocks are used for simple projects so you can't change the amount."
+let newsBox = "<a href='https://github.com/nmsderp'>nmsderp</a> has added functionality to the move/move back blocks, these blocks are used for simple projects so you can't change the amount."
 let baseUrl = "https://bark-coding.vercel.app"
 
 let mobile = window.navigator.userAgent.toLowerCase().includes("mobi");
