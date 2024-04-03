77b1648d615c90e533454c4b92c4d1fe420eff04
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-04-03 15:36:32 -0700

    I'm prb gonna throw up 🤢

--- src/scripts/replacer.js
+++ src/scripts/replacer.js
@@ -25,13 +25,13 @@ function replace(text) {
         .replaceAll(/(kitten lip|strip club|pornograph|paedophile)+/gim, t(10))
         .replaceAll(/(kitty lip|anilingus|camel toe|masturbat|wet dream|stripper|pedophile|marijuana|instagram|slaughter)+/gim, t(9))
         .replaceAll(/(dick pic|ball sac|pleasure|oral sex|anal sex|creampie|cum shot|fellatio|she-male|she male|tamakeri|cameltoe|ejaculat|find you|jerk off|assmunch|babeland|god damn|facebook|snapchat)+/gim, t(8))
-        .replaceAll(/(shut up|shaddup|goddamn|god dam|the hub|pornhub|tea bag|scrotum|ballsac|bukkake|facesit|fluffer|shemale|upskirt|mammary|quickie|undress|gay sex|erotism|eat ass|asshole|bastard|cumming|twitter|discord|patreon)+/gim, t(7))
+        .replaceAll(/(shut up|shaddup|goddamn|god dam|the hub|pornhub|tea bag|scrotum|ballsac|bukkake|facesit|fluffer|shemale|upskirt|mammary|quickie|undress|gay sex|erotism|eat ass|sexting|asshole|bastard|cumming|twitter|discord|patreon)+/gim, t(7))
         .replaceAll(/(penile|vagina|nipple|teabag|ekiben|erotic|tranny|sexcam|rimjob|raping|rapist|rectum|pantie|orgasm|nudity|faggot|incest|murder|hentai|retard|my ass|breast|tiktok|reddit|bereal|murder)+/gim, t(6))
         .replaceAll(/(bitch|pussy|whore|f off|effin|penis|erect|semen|lmfao|smash|cheek|seggs|teste|prick|squaw|thong|vulva|naked|horny|dildo|booty|boner|balls)+/gim, t(5))
         .replaceAll(/(fuck|shit|fock|cunt|cock|dick|gyat|boob|lmao|nude|porn|wank|slut|milf|dilf|yiff|yaoi|twat|tush|rape|pedo|nsfw|jizz|kike|jugg|kill|hump|tidd|2g1c|anus|tits|titt|crap)+/gim, t(4))
-        .replaceAll(/(p ⭐️|⬛️ 🟧|cbt|bbc|kys|fag|cum|kys)+/gim, t(3))
+        .replaceAll(/(p ⭐️|⬛️ 🟧|cbt|bbc|kys|fag|cum|kys|sex)+/gim, t(3))
         .replaceAll(/(p⭐️|⬛️🟧|69)+/gim, t(2))
-        .replaceAll(/(🤱|💦|🕳️|🤤|🍑|🥒|🍆|🖕|😩|😫|🥴|🚛|🍒|🍝|🌽|👅|🥵|🤓)+/gim, t(1))
+        .replaceAll(/(🤱|💦|🕳️|🤤|🍑|🥒|🍆|🖕|😩|😫|🥴|🚛|🍒|🍝|🌽|👅|🥵|🤓|👙|💃)+/gim, t(1))
         
         // emojis
         .replaceAll(":amazed:", "<img src='src/emojis/normal/svg/amazed.svg' style='margin-bottom: -7px;' alt='amazed face' width='"+ emojisize + "' height='"+ emojisize + "'>")
