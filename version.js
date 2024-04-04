9148884eec5e30a340ad14bea344fb6974e5fca2
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-04-04 15:54:25 -0700

    knick knack

--- version.js
+++
@@ -1,7 +0,0 @@
-fetch('https://bark-coding.vercel.app/package.json')
-    .then(res => res.json()).then(data => {
-    obj = data;})
-  .then(() => {
-      console.log(obj.version)
-      export const currentVersion = obj.version;
-   });
