954c73119ff08815398ebcb53e59c3ca5a79e2d8
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-04-07 10:36:27 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,5 @@
-from flask import Flask
+from flask import Flask, redirect 
 # this is for WAY LATER IN THE PROJECT, im just starting the source code out rn
+@app.route("/")
+def mainpage():
+  return redirect("https://bark-coding.vercel.app/")
