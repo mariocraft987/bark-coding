37e0c0426922f67d90fcb150b0e8260eb42e7302
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-04-18 09:08:45 -0700

    Update custom.js

--- editor_new/custom.js
+++ editor_new/custom.js
@@ -1,11 +1,37 @@
-Blockly.Blocks['block_type'] = {
+Blockly.Blocks['default'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("lorem ipsum");
+        .appendField("Block name");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(255);
- this.setTooltip("Test block.");
+ this.setTooltip("lorem ipsum");
+ this.setHelpUrl("");
+  }
+};
+Blockly.Blocks['create_elm'] = {
+  init: function() {
+    this.appendEndRowInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("create element")
+        .appendField(new Blockly.FieldTextInput("dog"), "NAME");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(0);
+ this.setTooltip("test");
+ this.setHelpUrl("");
+  }
+};   
+Blockly.Blocks['delete_elm'] = {
+  init: function() {
+    this.appendEndRowInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("delete element")
+        .appendField(new Blockly.FieldTextInput("dog"), "NAME");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(0);
+ this.setTooltip("test");
  this.setHelpUrl("");
   }
 };
