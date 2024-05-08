c4fb9ea5dda3b4218a7da3f94395197e9572151e
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-05-08 09:09:33 -0700

    delete

--- index.js
+++ index.js
@@ -54,25 +54,6 @@ function whatsNew() {
     }
 }
 
-function recentCom() {
-    document.getElementById("boxChanger").innerHTML = "<h2>Fetching commits...</h2><p>please wait...</p>"; // get it? "fetching" commits??? eh????........anyone?
-    fetch('https://api.github.com/repos/Mariocraft987/bark.github.io/commits?per_page=50')
-        .then(response => response.json())
-        .then(commits => {
-            var Commits = '<h2>Recent Commits</h2><ul style="overflow-y: scroll; height: 150px;"><br/>';
-            commits.slice(0, commitsLength).forEach(commit => {
-                Commits += `<div title="${replace(commit.author.login)}: ${replace(commit.commit.message)}"><li><a href="https://github.com/${replace(commit.author.login)}"><img style="border-radius:12px;margin-top:-4px;margin-left:-6px" src="https://github.com/${commit.author.login}.png" width="21"></a><a class="linkoncommits" href="${commit.html_url}">${replace(commit.commit.message)}</a></li></div>`;
-            });
-            Commits += '</ul><br><a class="buttonFrBx" href="https://github.com/Mariocraft987/bark.github.io/commits/main/" style="text-align: center;">See all</a><br>';
-            finalCommits = Commits.replaceAll(":trollface:", "<img src='src/emojis/svg/troll.svg' width='23'>")
-            document.getElementById("boxChanger").innerHTML = finalCommits + "<br/>"
-        })
-        .catch(error => {
-            console.error('Error fetching commits:', error);
-            document.getElementById("boxChanger").innerHTML = '<h2>Oops... :(</h2><p>Failed to load commits. Check your connection and try again.</p>';
-        });
-}
-
 function randomTxt() {
     //geeksforgeeks.org for the random text generator.
 
