da806b5b1da7bd00218f07cb69a6e8d5d44d93bd
Bashamega <adambashaahmednaji@gmail.com>
2024-05-10 18:53:46 +0300

    Local Storage integration

---
+++ settings/settings.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('navColor');
+    if (form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const val = document.getElementById('navbar_color').value;
+            if (val) {
+                localStorage.setItem('theme', val);
+                window.location.reload();
+            }
+        })
+    }
+})
+var pendingTheme = localStorage.getItem('theme')
+const btns = Array.from(document.getElementsByClassName('themeBtn'));
+btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        pendingTheme = e.target.name
+    });
+});
+const applybtn = document.getElementById('applybtn');
+applybtn.addEventListener('click', () => {
+    localStorage.setItem('theme', pendingTheme);
+    window.location.reload();
+})
