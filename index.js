2765eef9cf1d54ff79c1a1e5c0ba00386092b5a0
Adam Basha <110662505+Bashamega@users.noreply.github.com>
2024-05-11 11:00:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -124,9 +124,8 @@ if (theme) {
         link.href = `https://bark-coding.vercel.app/src/themes/${theme}.css`;
         document.head.append(link);
     };
-}else{
-    const link = document.createElement('link');
-    link.rel = "stylesheet";
-    link.href = `https://bark-coding.vercel.app/src/themes/bluedodger.css`;
-    document.head.append(link);
 };
+const link = document.createElement('link');
+link.rel = "stylesheet";
+link.href = `https://bark-coding.vercel.app/src/themes/bluedodger.css`;
+document.head.append(link);
