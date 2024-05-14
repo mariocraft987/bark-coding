0f00b95df371aede7dd6728df64012419679c0e9
Sihan <29PathiS@mbbc.qld.edu.au>
2024-05-15 07:40:48 +1000

    updated code

--- index.js
+++ index.js
@@ -134,7 +134,6 @@ if (theme) {
         document.head.append(link);
     }
 
-// Get the button:
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -149,7 +148,7 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+function scrollToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
