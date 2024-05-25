3fec619811eab04302e504e0b36b9b8444023a7c
qwertyy <qwertyytheartist@gmail.com>
2024-05-24 17:11:44 -0700

    no submit

--- settings/settings.js
+++ settings/settings.js
@@ -13,17 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
             btn.classList.add('active');
         };
     };
-    const form = document.getElementById('navColor');
-    if (form) {
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            const val = document.getElementById('navbar_color').value;
-            if (val) {
-                localStorage.setItem('theme', val);
-                window.location.reload();
-            }
-        })
-    }
 })
 var pendingTheme = localStorage.getItem('theme');
 const btns = Array.from(document.getElementsByClassName('themeBtn'));
@@ -37,7 +26,12 @@ btns.forEach(btn => {
         const newBtn = document.querySelector('[name="' + pendingTheme + '"]');
         if (newBtn) {
             newBtn.classList.add('active');
-        } 
+        }
+        const val = document.getElementById('navbar_color').value;
+        if (val) {
+           localStorage.setItem('theme', val);
+            window.location.reload();
+        }
     });
 });
 const applybtn = document.getElementById('applybtn');
