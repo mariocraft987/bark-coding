f488a2bf9c12236020eda5c42b2168c57b3fbfca
qwertyy <qwertyytheartist@gmail.com>
2024-06-01 10:18:39 -0700

    why is it now showing

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,4 +1,4 @@
-content = '<div class="navbar"><a href="' + baseUrl + '"><img src="' + window.location.origin +'/src/images/Logo.svg" alt="bark" width="25" height="25" ><img src="' + window.location.origin +'/src/emojis/svg/pride.svg" alt="pride flag" width="25" height="25" ></a>'
+content = '<div class="navbar"><a href="' + baseUrl + '"><img src="' + window.location.origin +'/src/images/Logo.svg" alt="bark" width="25" height="25"><img src="' + window.location.origin +'/src/emojis/svg/pride.svg" alt="pride flag" width="25" height="25"></a>'
 content += '<a href="/editor">Create</a>'
 content += '<a href="/explore">Explore</a>'
 content += '<a href="/ideas">Ideas</a>' 
