90c0435871e78b12bf36d2c55896877f7b89c710
dumorando <dumorando@proton.me>
2024-06-23 01:22:25 -0700

    mess around and fix a few things

--- editor/extensions.js
+++ editor/extensions.js
@@ -1,58 +1,22 @@
 // Bark extension handler
 
-//promise to wait for window.workspace to be available
-function waitForWorkspace() {
-    return new Promise((resolve, reject) => {
-        let elapsedTime = 0;
-
-        const checkWorkspace = () => {
-            if (window.workspace !== undefined) {
-                resolve(window.workspace);
-            } else {
-                elapsedTime += 100;
-                if (elapsedTime >= 10000) {
-                    reject(new Error("lol we waited too long"));
-                } else {
-                    setTimeout(checkWorkspace, 100);
-                }
-            }
-        };
-
-        checkWorkspace();
-    });
-}
-
 const sineXML = `
-<category name="Sine" colour="#19b9c2">
         <block type="sine_move"></block>
         <block type="sine_move2"></block>
         <block type="sine_size"></block>
-      </category>
 `;
 
 const aiXML = `
-<category name="A.I." colour="#5E7D6C">
         <block type="set_chatbot"></block>
         <block type="chatbot_respond"></block>
         <block type="chatbot_tell"></block>
         <block type="chatbot_reset"></block>
-      </category>
 `;
 
 const customXML = `
-      <category name="Custom" colour="#cf4125">
         <block type="create_elm"></block>
         <block type="delete_elm"></block>
         <block type="get_elm"></block>
-        </category>
-`;
-
-const sineXML = `
-<category name="Sine" colour="#19b9c2">
-        <block type="sine_move"></block>
-        <block type="sine_move2"></block>
-        <block type="sine_size"></block>
-      </category>
 `;
 
 
@@ -74,15 +38,14 @@ const state = {
 Object.keys(MapIdToExtensions).forEach(item => {
     document.getElementById(item).addEventListener("click", () => {
         if (!state.extensionsLoaded.includes(item)) {
-            //adds the ext to the toolbox xml
-            document.getElementById("toolbox").innerHTML += MapIdToExtensions[item];
-            //updates the blockly toolbox to match the new xml
-            waitForWorkspace()
-                .then(workspace => {
-                    workspace.updateToolbox(document.getElementById("toolbox").outerHTML);
-                    //push it to state so you cant add it twice :bleh:
-                    state.extensionsLoaded.push(item);
-                });
+            //push it to state so you cant add it twice :bleh:
+            state.extensionsLoaded.push(item);
         }
     });
 });
+
+function extflyoutcallback(workspace) {
+    return state.extensionsLoaded.map(item => MapIdToExtensions[item]);
+}
+
+setTimeout(() => Blockly.getMainWorkspace().registerToolboxCategoryCallback('EXTS', extflyoutcallback), 2000);
