9c80724dd0b343419d4f845cf4f8fc7f08bf08ea
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-06-23 10:07:32 -0700

    fix

--- src/scripts/usernameGen.js
+++ src/scripts/usernameGen.js
@@ -5,8 +5,15 @@ function generateUsername() {
     "Robot",
     "Dog",
     "Cat",
-    "Coder"
+    "Coder",
+    "Miner",
+    "Cool",
+    "Funny",
+    "Hampster",
+    "Hat",
+    "Rapster",
+    "Cereal"
   ]
-  let name = selectedWords[Math.floor(Math.random() * selectedWords.length)]} + selectedWords[Math.floor(Math.random() * selectedWords.length)]} + Math.floor(Math.random() * 99);
+  let name = selectedWords[Math.floor(Math.random() * selectedWords.length)] + selectedWords[Math.floor(Math.random() * selectedWords.length)] + Math.floor(Math.random() * 99);
   return name;
 }
