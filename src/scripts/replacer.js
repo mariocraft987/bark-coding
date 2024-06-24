f65bcfcd4b3e7ed6f9ac903b16a1e5102bd1307c
dumorando <144301348+dumorando@users.noreply.github.com>
2024-06-23 21:12:33 -0700

    fix some things

--- src/scripts/replacer.js
+++ src/scripts/replacer.js
@@ -6,162 +6,162 @@ const emojis = [
     {
         token: ":actingSus:",
         emoji: "actingSus",
-        url: "src/emojis/svg/normal/actingSus.svg"
+        url: "/src/emojis/svg/normal/actingSus.svg"
     },
     {
         token: ":amazed:",
         emoji: "amazed",
-        url: "src/emojis/svg/normal/amazed.svg"
+        url: "/src/emojis/svg/normal/amazed.svg"
     },
     {
         token: ":amongus:",
         emoji: "amongus",
-        url: "src/emojis/svg/joke/amongus.svg"
+        url: "/src/emojis/svg/joke/amongus.svg"
     },
     {
         token: ":alien:",
         emoji: "alien",
-        url: "src/emojis/svg/normal/alien.svg"
+        url: "/src/emojis/svg/normal/alien.svg"
     },
     {
         token: ":smiley:",
         emoji: "smiley",
-        url: "src/emojis/svg/normal/smiley.svg"
+        url: "/src/emojis/svg/normal/smiley.svg"
     },
     {
         token: ":happy:",
         emoji: "happy",
-        url: "src/emojis/svg/normal/happy.svg"
+        url: "/src/emojis/svg/normal/happy.svg"
     },
     {
         token: ":sad:",
         emoji: "sad",
-        url: "src/emojis/svg/normal/sad.svg"
+        url: "/src/emojis/svg/normal/sad.svg"
     },
     {
         token: ":angry:",
         emoji: "angry",
-        url: "src/emojis/svg/normal/angry.svg"
+        url: "/src/emojis/svg/normal/angry.svg"
     },
     {
         token: ":raisedEyebrow:",
         emoji: "raisedEyebrow",
-        url: "src/emojis/svg/normal/raiseEyebrow.svg"
+        url: "/src/emojis/svg/normal/raiseEyebrow.svg"
     },
     {
         token: ":loading:",
         emoji: "loading",
-        url: "src/emojis/svg/normal/loading.svg"
+        url: "/src/emojis/svg/normal/loading.svg"
     },
     {
         token: ":lol:",
         emoji: "lol",
-        url: "src/emojis/svg/normal/lol.svg"
+        url: "/src/emojis/svg/normal/lol.svg"
     },
     {
         token: ":textHappy:",
         emoji: "textHappy",
-        url: "src/emojis/svg/normal/textHappy.svg"
+        url: "/src/emojis/svg/normal/textHappy.svg"
     },
     {
         token: ":sussedOut:",
         emoji: "sussedOut",
-        url: "src/emojis/svg/normal/sussedOut.svg"
+        url: "/src/emojis/svg/normal/sussedOut.svg"
     },
     {
         token: ":goofy:",
         emoji: "goofy",
-        url: "src/emojis/svg/normal/goofy.svg"
+        url: "/src/emojis/svg/normal/goofy.svg"
     },
     {
         token: ":mad:",
         emoji: "mad",
-        url: "src/emojis/svg/normal/mad.svg"
+        url: "/src/emojis/svg/normal/mad.svg"
     },
     {
         token: ":joyful:",
         emoji: "joyful",
-        url: "src/emojis/svg/normal/joyful.svg"
+        url: "/src/emojis/svg/normal/joyful.svg"
     },
     {
         token: ":moneyEyes:",
         emoji: "moneyEyes",
-        url: "src/emojis/svg/normal/moneyEyes.svg"
+        url: "/src/emojis/svg/normal/moneyEyes.svg"
     },
     {
         token: ":evil:",
         emoji: "evil",
-        url: "src/emojis/svg/normal/evil.svg"
+        url: "/src/emojis/svg/normal/evil.svg"
     },
     {
         token: ":panic:",
         emoji: "panic",
-        url: "src/emojis/svg/normal/panic.svg"
+        url: "/src/emojis/svg/normal/panic.svg"
     },
     {
         token: ":dizzy:",
         emoji: "dizzy",
-        url: "src/emojis/svg/normal/dizzy.svg"
+        url: "/src/emojis/svg/normal/dizzy.svg"
     },
     {
         token: ":jealous:",
         emoji: "jealous",
-        url: "src/emojis/svg/normal/jealous.svg"
+        url: "/src/emojis/svg/normal/jealous.svg"
     },
     {
         token: ":smallsmile:",
         emoji: "smallsmile",
-        url: "src/emojis/svg/normal/smallsmile.svg"
+        url: "/src/emojis/svg/normal/smallsmile.svg"
     },
     {
         token: ":troll:",
         emoji: "troll",
-        url: "src/emojis/svg/normal/troll.svg"
+        url: "/src/emojis/svg/normal/troll.svg"
     },
     {
         token: ":fire:",
         emoji: "fire",
-        url: "src/emojis/svg/normal/fire.svg"
+        url: "/src/emojis/svg/normal/fire.svg"
     },
     {
         token: ":pride:",
         emoji: "pride",
-        url: "src/emojis/svg/normal/pride.svg"
+        url: "/src/emojis/svg/normal/pride.svg"
     },
     {
         token: ":rofl:",
         emoji: "rofl",
-        url: "src/emojis/svg/normal/rofl.svg"
+        url: "/src/emojis/svg/normal/rofl.svg"
     },
     {
         token: ":skibidi:",
         emoji: "skibidi",
-        url: "src/emojis/svg/joke/skibidi.svg"
+        url: "/src/emojis/svg/joke/skibidi.svg"
     },
     {
         token: ":thumb:",
         emoji: "thumb",
-        url: "src/emojis/svg/normal/thumb.svg"
+        url: "/src/emojis/svg/normal/thumb.svg"
     },
     {
         token: ":robot:",
         emoji: "robot",
-        url: "src/emojis/svg/normal/robot.svg"
+        url: "/src/emojis/svg/normal/robot.svg"
     },
     {
         token: ":loading:",
         emoji: "loading",
-        url: "src/emojis/svg/normal/loading.svg"
+        url: "/src/emojis/svg/normal/loading.svg"
     },
     {
         token: ":clapAnim:",
         emoji: "clapAnim",
-        url: "src/emojis/svg/normal/clap_anim.gif"
+        url: "/src/emojis/svg/normal/clap_anim.gif"
     },
     {
         token: ":clap:",
         emoji: "clap",
-        url: "src/emojis/svg/normal/clap.svg"
+        url: "/src/emojis/svg/normal/clap.svg"
     },
     /*{
         token: ":emojiName:",
@@ -197,14 +197,14 @@ function replace(text) {
         .replaceAll(/(🤱|💦|🕳️|🤤|🍑|🍆|🖕|😩|😫|🍒|🥵|🤓|👙|🥒)+/gim, t(1))
 
         // emojis of dev
-        .replaceAll(":mariocraft:", "<img src='src/emojis/svg/dev/mariocraft.svg' alt='dev' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
-        .replaceAll(":qwertyy:", "<img src='src/emojis/svg/dev/qwertyy.png' alt='dev'' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
-        .replaceAll(":rubiidev:", "<img src='src/emojis/svg/dev/rubiidev.svg' alt='dev' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
-        .replaceAll(":nmsderp:", "<img src='src/emojis/svg/dev/nmsderp.svg' alt='dev'' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
-        .replaceAll(":dumo:", "<img src='src/emojis/svg/dev/dumo.svg' alt='dev' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
+        .replaceAll(":mariocraft:", "<img src='/src/emojis/svg/dev/mariocraft.svg' alt='dev' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
+        .replaceAll(":qwertyy:", "<img src='/src/emojis/svg/dev/qwertyy.png' alt='dev'' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
+        .replaceAll(":rubiidev:", "<img src='/src/emojis/svg/dev/rubiidev.svg' alt='dev' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
+        .replaceAll(":nmsderp:", "<img src='/src/emojis/svg/dev/nmsderp.svg' alt='dev'' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
+        .replaceAll(":dumo:", "<img src='/src/emojis/svg/dev/dumo.svg' alt='dev' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
 
         // joke emojis
-        .replaceAll(":mewing:", "<img src='src/emojis/svg/joke/mewing.svg' alt='mewing' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
+        .replaceAll(":mewing:", "<img src='/src/emojis/svg/joke/mewing.svg' alt='mewing' style='margin-bottom: -7px;' width='" + emojisize + "' height='" + emojisize + "'>")
 
         // devoloper tags
         .replaceAll(/(@mariocraft987|@mariocraft)+/gim, '<a href="https://github.com/Mariocraft987" style="background-color: #988bbf;height: 16px;display: inline-block;padding: 5px;margin: -5px 3px;border: 1px solid #ebd7ff;border-radius: 10px;color: #ffffff;position: relative;top: 2px; text-decoration: none;"><img src="https://github.com/mariocraft987.png" width="16px" style="border-radius: 50%;"><span style="top: -2px;margin-left: 3px;margin-right: 3px;position: relative;">mariocraft</span></a>')
