95d0eb3fa4b8f56d354f1e74f35dc308b7e3fa30
Bashamega <adambashaahmednaji@gmail.com>
2024-06-28 18:08:23 +0300

    Settings page fix

--- settings/settings.js
+++ settings/settings.js
@@ -11,11 +11,7 @@ function checkDarkModeSelect() {
     }
 
 function switchTheme(selected) {
-    if (selected != "pranidhi", selected != "cherryblossom", selected != "bluedodger", selected !="shelledgastropod") {
-        localStorage.theme = selected;
-    }else{
-        localStorage.theme = "bluedodger";
-    }
+    localStorage.theme = selected;
     window.location.reload()
 }
     
