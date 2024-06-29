6dbdfb435707699bddd014f11aba1f18bf09eeeb
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-06-29 08:32:33 -0700

    setting only work for logged in

--- src/scripts/injector.js
+++ src/scripts/injector.js
@@ -1,5 +1,6 @@
 const actionsecret = localStorage.getItem("secret");
 let profile = !!localStorage.getItem("myBarkUsername") ? `<a href="/signout?code=${encodeURIComponent(actionsecret)}" class="right">Sign out</a><a href="/profile/${localStorage.getItem("myBarkUsername")}" class="right">My Profile</a></div>` : '<a href="login" class="right">Login</a><a href="signup" class="right">Join bark</a></div>'
+let setting = !!localStorage.getItem("myBarkUsername") ? `<a href="/settings">Settings</a>` : ''
 let afdbtn = new Date().getMonth() === 3 && new Date().getDate() === 1 ? "<a id='flip-it-afd-btn'>FLIP IT</a>" : '';
 const components = {
     //navbar
@@ -10,7 +11,7 @@ const components = {
     <a href="/ideas">Ideas</a>
     <a href="/about">About</a>
     `+ /*<a href="https://github.com/mariocraft987/bark.github.io/discussions">Discuss</a>*/ `
-    <a href="/settings">Settings</a>
+    ${setting}
     ${afdbtn}
     <a class="dark-mode-button" onclick="toggleDarkMode()" id="darkModeToggle"><dmbico alt="Dark Mode"></dmbico></a><form action="search" style="display: contents;"><input class="searchBar" id="search" name="q" placeholder="Search for Projects"></input></form>
     ${profile}
