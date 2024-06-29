7c473249eebccc32fc4ad833d8b977ad2a17e1ac
Bashamega <adambashaahmednaji@gmail.com>
2024-06-29 11:40:10 +0300

    Cleaning

--- src/scripts/admin.js
+++
@@ -1,8 +0,0 @@
-async function isuseradmin(username) {
-    const admins = await fetch('https://api.bark.dumorando.com/api/v2/admins').then(data => data.json());
-    return admins.includes(username);
-}
-
-async function amiadmin() {
-    return await isuseradmin(localStorage.getItem("myBarkUsername"));
-}
