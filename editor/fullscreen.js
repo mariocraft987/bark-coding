41fb65df6628eb6fb304d9c0c1a6c5cc7ba3c8c0
Mr. Bolts <154646419+mariocraft987@users.noreply.github.com>
2024-07-13 10:41:19 -0700

    Update fullscreen.js

--- editor/fullscreen.js
+++ editor/fullscreen.js
@@ -5,11 +5,15 @@ function NewtoggleFullscreen() {
     document.getElementById("blocklyDiv").style.display = "block";
     document.getElementById("xysize").style.display = "block";
     document.getElementById("editortabs").style.display = "block";
+    document.getElementById("defaultCanvas0").style.width = "640px"
+    document.getElementById("defaultCanvas0").style.height = "360px"
     isFullScreen = false
   }else{
     document.getElementById("blocklyDiv").style.display = "none";
     document.getElementById("xysize").style.display = "none";
     document.getElementById("editortabs").style.display = "none";
+    document.getElementById("defaultCanvas0").style.width = "710px"
+    document.getElementById("defaultCanvas0").style.height = "430px"
     isFullScreen = true
   }
 }
