744df6dcceed5bffdb9b0236c3a07975ec38f361
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-08-12 10:45:32 -0700

    Update navbar.js

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,20 +1,4 @@
-content = '<div class="navbar"><a href="' + baseUrl + '"><img src="' + window.location.origin +'/src/images/Logo.svg" alt="bark" width="25" height="25"><img src="' + window.location.origin +'/src/emojis/svg/pride.svg" alt="pride flag" width="25" height="25"></a>'
-content += '<a href="/editor">Create</a>'
-content += '<a href="/explore">Explore</a>'
-content += '<a href="/ideas">Ideas</a>' 
-content += '<a href="/about">About</a>'
-content += '<a href="https://github.com/mariocraft987/bark.github.io/discussions">Discuss</a>'
-content += '<a href="/settings">Settings</a>'
-// special april fools button ;)
-if (new Date().getMonth() === 3 && new Date().getDate() === 1) {
-    content += "<a id='flip-it-afd-btn'>FLIP IT</a>";
-}
-content += '<a class="dark-mode-button" onclick="toggleDarkMode()" id="darkModeToggle"><dmbico alt="Dark Mode"></dmbico></a><form action="search" style="display: contents;"><input class="searchBar" id="search" name="q" placeholder="Search for Projects"></input></form>'
-if (localStorage.getItem("myBarkUsername") == "") { // has profile?
-    content += '<a class="right">sign out</a><a class="right">profile</a>' // profile button
-} else {
-    content += '<a href="login" class="right">Sign in</a><a href="signup" class="right">Join bark</a></div>' // signup and login button
-}
+content = 'THIS IS OLD CODE FROM NAVBAR.JS, REFRESH THE PAGE TO CHANGE IT BACK, THIS WILL BE FIXED SOON'
 try {
     document.getElementById('navbar').innerHTML = content;
 } catch {
