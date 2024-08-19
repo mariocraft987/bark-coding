320068d2f19c85306772153a20ee7e7361ead6d9
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-08-19 10:10:59 -0700

    Update contrast.js

--- editor/contrast.js
+++ editor/contrast.js
@@ -1,12 +1,7 @@
-const logiccolor = "#cf9100";
-const motioncolor = "#3399ff";
-const controlcolor = "#689c00";
-const mathcolor = "#1b85d1";
-const textcolor = "#21d9b7";
-const varcolor = "#f2951b";
-const listcolor = "#f2461b";
-const funccolor = "#e643d2";
-const advancolor = "#0fbd8c";
-const customcolor = "#a826ff";
+// All list colors are sorted by order in the category orders
+
+const normalcolors = ["#cf9100", "#3399ff", "#689c00", "#1b85d1", "#21d9b7", "#f2951b", "#f2461b", "#e643d2", "#0fbd8c", "#a826ff"]
+
+const contrastcolors = ["#ffce5c", "#8ac4ff", "#c6ff54", "#51aced", "#91ffeb", "#ffc170", "#ff8b6e", "#ff94f2", "#7dfad7", "#d08aff"]
 
 document.getElementById("blockly-0").style.backgroundColor = "blue";
