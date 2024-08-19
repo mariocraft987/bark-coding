a8c545a47f150946e6261d1ecbc7f1b854b2601a
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-08-19 09:38:41 -0700

    Create contrast.js

---
+++ editor/contrast.js
@@ -0,0 +1,12 @@
+const logiccolor = "#cf9100";
+const motioncolor = "#3399ff";
+const controlcolor = "#689c00";
+const mathcolor = "#1b85d1";
+const textcolor = "#21d9b7";
+const varcolor = "#f2951b";
+const listcolor = "#f2461b";
+const funccolor = "#e643d2";
+const advancolor = "#0fbd8c";
+const customcolor = "#a826ff";
+
+document.getElementById("blockly-0").style.backgroundColor = "blue";
