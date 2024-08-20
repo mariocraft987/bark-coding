668a03dd5339e4800bdd9e3f21bf792fbd4ebc05
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-08-20 08:45:55 -0700

    Update contrast.js

--- editor/contrast.js
+++ editor/contrast.js
@@ -1,7 +1,19 @@
 // All list colors are sorted by order in the category orders
 
+let catelength = 10;
+
 const normalcolors = ["#cf9100", "#3399ff", "#689c00", "#1b85d1", "#21d9b7", "#f2951b", "#f2461b", "#e643d2", "#0fbd8c", "#a826ff"]
 
 const contrastcolors = ["#ffce5c", "#8ac4ff", "#c6ff54", "#51aced", "#91ffeb", "#ffc170", "#ff8b6e", "#ff94f2", "#7dfad7", "#d08aff"]
 
-document.getElementById("blockly-0").style.backgroundColor = "blue";
+function contrastBlocks(trueorfalse)
+// true = blocks on contrast, false = blocks on normal color
+for (let i = 0; i < catelength; i++) {
+  if (trueorfalse == true) {
+    document.getElementById("blockly-" + i).style.backgroundColor = normalcolors[i];
+    document.getElementById("blockly-" + i).style.color = "white";
+  } else {
+    document.getElementById("blockly-" + i).style.backgroundColor = contrastcolors[i];
+    document.getElementById("blockly-" + i).style.color = "black";
+  }
+} 
