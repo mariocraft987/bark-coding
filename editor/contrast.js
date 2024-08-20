571f1ddde39a43292ed27d4841b7f0b74659a105
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-08-20 09:19:44 -0700

    Update contrast.js

--- editor/contrast.js
+++ editor/contrast.js
@@ -1,11 +1,11 @@
 // All list colors are sorted by order in the category orders
 
+function contrastBlocks(trueorfalse) {
+// true = blocks on contrast, false = blocks on normal color
+
 let normalcolors = ["#cf9100", "#3399ff", "#689c00", "#1b85d1", "#21d9b7", "#f2951b", "#f2461b", "#e643d2", "#0fbd8c", "#a826ff"]
 
 let contrastcolors = ["#ffce5c", "#8ac4ff", "#c6ff54", "#51aced", "#91ffeb", "#ffc170", "#ff8b6e", "#ff94f2", "#7dfad7", "#d08aff"]
-
-function contrastBlocks(trueorfalse) {
-// true = blocks on contrast, false = blocks on normal color
 
 for (let i = 0; i < 10; i++) {
   if (trueorfalse == true) {
