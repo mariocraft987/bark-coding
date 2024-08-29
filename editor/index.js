989c1fc34d85891a7ec9b7d6318f461f50129e47
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-08-29 12:57:55 -0700

    i wanna try something

--- editor/index.js
+++ editor/index.js
@@ -5,5 +5,6 @@ function start() {
   window.workspace = Blockly.inject('blocklyDiv', {
     toolbox: document.getElementById('toolbox-categories'),
     renderer: 'zelos',
+    theme: 'classic',
   });
 }
