33b5d3405a1aca357150d00448423c4c1e5dbadc
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-09-10 11:09:47 -0700

    Update advanced.js

--- editor/cates/advanced.js
+++ editor/cates/advanced.js
@@ -21,7 +21,7 @@ Blockly.Blocks['letters_from'] = {
         .appendField(new Blockly.FieldNumber(5, 0, 999), "LAST")
         .appendField("of");
     this.setOutput(true, "String");
-    this.setColour(150);
+    this.setColour(160);
  this.setTooltip("Returns the string from 2 number inputs in a variable.");
  this.setHelpUrl("https://scratch.mit.edu/users/legoer3/#comments");
   }
