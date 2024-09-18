ad01bfc3ffbd08404bd14cc8d350647526d295a2
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-09-17 17:29:24 -0700

    change usernameGen.js

--- src/scripts/usernameGen.js
+++ src/scripts/usernameGen.js
@@ -1,5 +1,6 @@
 function generateUsername() {
   let selectedWords = [
+    "Bark",
     "Coder",
     "Kitty",
     "Clover",
@@ -20,7 +21,10 @@ function generateUsername() {
     "Toast",
     "Apple",
     "Lemon",
-    "Bowtie"
+    "Bowtie",
+    "Writing",
+    "Chess",
+    "Snail"
   ]
   let name = selectedWords[Math.floor(Math.random() * selectedWords.length)] + selectedWords[Math.floor(Math.random() * selectedWords.length)] + Math.floor(Math.random() * 99);
   return name;
