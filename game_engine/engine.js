f357d0d40aeb4e9af4cbfc6201f3c10637cce0c7
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-09-19 08:14:17 -0700

    ico

--- game_engine/engine.js
+++ game_engine/engine.js
@@ -1,5 +1,13 @@
 // hi :)
 
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+}
+link.href = '/game_engine/bicon.ico';
+
 function starteditor() {
     console.log("Running editor...")
 }
