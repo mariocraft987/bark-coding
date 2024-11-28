ecfe2201d76f2f072d586d7c477dd36b6f3cd000
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-11-28 09:14:55 -0700

    🦃

--- src/scripts/injector.js
+++ src/scripts/injector.js
@@ -2,6 +2,7 @@ const actionsecret = localStorage.getItem("secret");
 let profile = !!localStorage.getItem("myBarkUsername") ? `<a href="/mail" class="right">Mail</a><a href="/profile/${localStorage.getItem("myBarkUsername")}" class="right">My Profile</a></div>` : '<a href="login" class="right">Login</a><a href="signup" class="right">Join bark</a></div>'
 let setting = !!localStorage.getItem("myBarkUsername") ? `<a href="/settings">Settings</a>` : ''
 let afdbtn = new Date().getMonth() === 3 && new Date().getDate() === 1 ? "<a id='flip-it-afd-btn'>FLIP IT</a>" : '';
+let turkey = new Date().getMonth() === 10 && new Date().getDate() === 28 ? "<a onclick='alert(`Happy Thanksgiving!!\n\n🦃🦃🦃`)'>🦃</a>" : '';
 const components = {
     //navbar
     "navbar": `
@@ -11,6 +12,7 @@ const components = {
     <a href="/about">About</a>
     ${setting}
     ${afdbtn}
+    ${turkey}
     <a class="dark-mode-button" onclick="toggleDarkMode()" id="darkModeToggle"><dmbico alt="Dark Mode"></dmbico></a><form action="search" style="display: contents;"><input class="searchBar" id="search" name="q" placeholder="Search for Projects"></input></form>
     ${profile}
     `,
