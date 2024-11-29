aa27a729daa4d70db556e51f311fdbdf69428b9a
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-11-29 14:22:44 -0700

    hehe

---
+++ src/scripts/barkgame.js
@@ -0,0 +1,11 @@
+num = 0;
+document.getElementById("bigbrainman").addEventListener("click", ctbd_addPlus);
+
+// ctbd for Catch that bark dog
+// https://github.com/mariocraft987/bark-coding/discussions/264
+function ctbd_addPlus() {
+    num++;
+    if (num == 7) {
+        alert("( ͡° ͜ʖ ͡°)")
+    }
+}
