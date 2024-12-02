8189f07bb936bb760b6418be322ccfa0507161a8
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-12-02 15:09:30 -0700

    ditch colored text

--- blockly_editor/advanced/script.js
+++ blockly_editor/advanced/script.js
@@ -10,19 +10,6 @@ let bluroverlay = document.getElementById("blur-overlay");
 
 code.focus();
 
-function colorTextEditor() {
-    // SUPER optimized code yea yeah
-
-    newcode = document.getElementById("code").innerHTML;
-    document.getElementById("code").innerHTML = newcode.replaceAll("let", "<magenta>let</magenta>");
-    newcode = document.getElementById("code").innerHTML;
-    document.getElementById("code").innerHTML = newcode.replaceAll("draw()", "<lime>draw()</lime>");
-    newcode = document.getElementById("code").innerHTML;
-    document.getElementById("code").innerHTML = newcode.replaceAll("new", "<orange>new</orange>");
-}
-
-colorTextEditor()
-
 let zoom = function() {
     if (zoomsize < 50) {
         zoomsize += 4;
