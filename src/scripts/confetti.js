65198c2533320ec1b5316c61057ad158edad59e7
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-12-03 08:51:24 -0700

    ehhe

--- src/scripts/confetti.js
+++ src/scripts/confetti.js
@@ -1,5 +1,11 @@
-$('head').append('<link rel="stylesheet" type="text/css" href="https://bark.dumorando.com/src/css/confetti.scss">')
-$('body').append(`<div class="confetti">
+var link = $("<link />",{
+    rel: "stylesheet",
+    type: "text/css",
+    href: "https://bark.dumorando.com/src/css/confetti.scss"
+  })
+  $('head').append(link);
+
+document.body.innerHTML += `<div class="confetti">
   <div class="confetti-piece"></div>
   <div class="confetti-piece"></div>
   <div class="confetti-piece"></div>
@@ -13,6 +19,6 @@ $('body').append(`<div class="confetti">
   <div class="confetti-piece"></div>
   <div class="confetti-piece"></div>
   <div class="confetti-piece"></div>
-</div>`)
+</div>`;
 
 // https://stackoverflow.blog/2021/05/31/shipping-confetti-to-stack-overflows-design-system/
