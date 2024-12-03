4173b877ec86b472dd4ea27dc0191232a667ba21
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-12-03 08:49:00 -0700

    confet EEEEE

---
+++ src/scripts/confetti.js
@@ -0,0 +1,18 @@
+$('head').append('<link rel="stylesheet" type="text/css" href="https://bark.dumorando.com/src/css/confetti.scss">')
+$('body').append(`<div class="confetti">
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+  <div class="confetti-piece"></div>
+</div>`)
+
+// https://stackoverflow.blog/2021/05/31/shipping-confetti-to-stack-overflows-design-system/
