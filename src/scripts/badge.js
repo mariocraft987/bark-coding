6ebb7643310970b6f8d7539cfa49360286667695
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-12-05 17:51:04 -0700

    Update badge.js

--- src/scripts/badge.js
+++ src/scripts/badge.js
@@ -1,6 +1,9 @@
+function image_badge(url, name) {
+    return `<img src="${url}" title="${url}" alt="${url} badge" width="36" style="image-rendering: pixelated;">`;
+}
 
 function setup_badges(name) {
     if (name = "marioc") {
-        document.getElementById("username").outerHTML += '<img src="https://bark.dumorando.com/src/images/assets/badges/b_dog.png" width="36" style="image-rendering: pixelated;">';
+        document.getElementById("username").outerHTML += image_badge("https://bark.dumorando.com/src/images/assets/badges/b_dog.png", "Dogggggg");
     }
 }
