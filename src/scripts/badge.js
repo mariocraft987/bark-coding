aa0c05861b08c621c242d5a8e1da6ca41d5c1e0a
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-12-06 17:15:15 -0700

    Update badge.js

--- src/scripts/badge.js
+++ src/scripts/badge.js
@@ -3,7 +3,13 @@ function image_badge(url, name) {
 }
 
 function setup_badges(name) {
-    if (name = "marioc") {
+    if (name == "marioc") {
         document.getElementById("username").outerHTML += image_badge("https://bark.dumorando.com/src/images/assets/badges/b_dog.png", "Dogggggg");
+    }
+    if (name == "marioc" || name == "breakfast" || name == "dumo" || name == "nmsderp" || name == "Rubii484Bark") {
+        document.getElementById("username").outerHTML += image_badge("https://bark.dumorando.com/src/images/assets/badges/b_dev.png", "DEVELOPERRR");
+    }
+    if (name == "RarestUsername") {
+        document.getElementById("username").outerHTML += image_badge("https://bark.dumorando.com/src/images/assets/badges/b_bruh.png", "Bruh");
     }
 }
