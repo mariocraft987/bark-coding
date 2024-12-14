15872b84ce5c6236e0bd66975edab2f254f9d67b
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-12-14 12:42:23 -0700

    Test

--- src/scripts/badge.js
+++ src/scripts/badge.js
@@ -2,14 +2,24 @@ function image_badge(url, name) {
     return `<a href="https://bark.dumorando.com/docs#/Badges"><img src="${url}" title="${name} Badge" alt="has ${name} badge" width="36" style="image-rendering: pixelated; cursor: help;"></a>`;
 }
 
+function badge_type(e) {
+    if (e == 1) {
+        document.getElementById("username").outerHTML += image_badge("https://bark.dumorando.com/src/images/assets/badges/b_dog.png", "Dogggggg");
+    } else if (e == 2) {
+        document.getElementById("username").outerHTML += image_badge("https://bark.dumorando.com/src/images/assets/badges/b_dev.png", "DEVELOPERRR");
+    } else if (e == 3) {
+        document.getElementById("username").outerHTML += image_badge("https://bark.dumorando.com/src/images/assets/badges/b_bruh.png", "Bruh");
+    }
+}
+
 function setup_badges(name) {
     if (name == "marioc") {
-        document.getElementById("username").outerHTML += image_badge("https://bark.dumorando.com/src/images/assets/badges/b_dog.png", "Dogggggg");
+        badge_type(1);
     }
     if (name == "marioc" || name == "breakfast" || name == "dumo" || name == "nmsderp" || name == "Rubii484Bark") {
-        document.getElementById("username").outerHTML += image_badge("https://bark.dumorando.com/src/images/assets/badges/b_dev.png", "DEVELOPERRR");
+        badge_type(2);
     }
     if (name == "RarestUsername") {
-        document.getElementById("username").outerHTML += image_badge("https://bark.dumorando.com/src/images/assets/badges/b_bruh.png", "Bruh");
+        badge_type(3);
     }
 }
