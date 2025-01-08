ca5f6313481b8308b6ddacd15169c44c1e8227b0
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2025-01-08 13:02:24 -0700

    yes, because i worked on bark before 2024, and...
    I REMEMBER BEING ON GOLDOT (personal project) ON NEW YEARS DAY IN 2023 AND I BASED GOLDOT OFF OF BARK

--- src/scripts/injector.js
+++ src/scripts/injector.js
@@ -38,8 +38,7 @@ const components = {
     <br/><br/><a href="https://bark.dumorando.com/docs">Docs</a>
     <a href="https://discord.gg/hXmHw7H9BF">Discord</a>
     <a href="https://x.com/barkcoding">Twitter</a></p>
-    <p style="color: #777;">bark dog now has the bone, we have no more bones left.</p>
-    <p style="font-size: 9pt; color: #777;">©2024 - 2025 Bark Coding<br/>
+    <p style="font-size: 9pt; color: #777;">©2023 - 2025 Bark Coding<br/>
     Even though this project is hosted on a dumorando.com domain, this project is barely affiliated with dumorando.</p></footer>
     `
 };
