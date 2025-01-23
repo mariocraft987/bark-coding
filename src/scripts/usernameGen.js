f9e35b94d957fd9c19ec86b724e5394c72589e23
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2025-01-23 12:36:29 -0700

    Update usernameGen.js

--- src/scripts/usernameGen.js
+++ src/scripts/usernameGen.js
@@ -28,7 +28,12 @@ function generateUsername() {
     "Craft",
     "Nice",
     "God",
-    "Fish"
+    "Fish",
+    "Banana",
+    "Cereal",
+    "Nice",
+    "Extremze",
+    "Skater"
   ]
   let name = selectedWords[Math.floor(Math.random() * selectedWords.length)] + selectedWords[Math.floor(Math.random() * selectedWords.length)] + Math.floor(Math.random() * 99);
   return name;
@@ -36,10 +41,4 @@ function generateUsername() {
 
 let generatedName = generateUsername();
 
-const newbtn = document.createElement('button');
-newbtn.addEventListener("click", () => document.getElementById('username').value = generatedName);
-newbtn.innerText = `How about ${generatedName}?`;
-newbtn.classList.add('buttonFrBx');
-newbtn.innerHTML += '<br /><br />';
-
-// document.getElementById('userGen').appendChild(newbtn);
+document.getElementById('username').placeholder = generatedName;
