7ee49b90cac6e0df8388d2d2877bd1d7fe07bb34
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2025-01-23 12:37:07 -0700

    change it to 900

--- src/scripts/usernameGen.js
+++ src/scripts/usernameGen.js
@@ -35,7 +35,7 @@ function generateUsername() {
     "Extremze",
     "Skater"
   ]
-  let name = selectedWords[Math.floor(Math.random() * selectedWords.length)] + selectedWords[Math.floor(Math.random() * selectedWords.length)] + Math.floor(Math.random() * 99);
+  let name = selectedWords[Math.floor(Math.random() * selectedWords.length)] + selectedWords[Math.floor(Math.random() * selectedWords.length)] + Math.floor(Math.random() * 900);
   return name;
 }
 
