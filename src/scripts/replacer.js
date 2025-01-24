818178fc849d7c9f557d685631a4ca7cd30dba01
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2025-01-24 10:00:15 -0700

    oh hell no

--- src/scripts/replacer.js
+++ src/scripts/replacer.js
@@ -232,8 +232,9 @@ function replace(text) {
         .replaceAll(/(cumming|asshole|bestial|cocaine|jackass)+/gim, t(7))
         .replaceAll(/(penile|virgin|breast|condom|faggot|incest|molest|retard|rapist|hentai|orgasm|nudity)+/gim, t(6))
         .replaceAll(/(bitch|pussy|penis|vagin|prick|whore|booty|boner|erect|horny|skank|sperm|teste|eroti|balls|semen|ligma)+/gim, t(5))
-        .replaceAll(/(fuck|dick|cock|cunt|nigg|slut|porn|boob|jizz|milf|dilf|pimp|rape|sext|nude|tits|titt)+/gim, t(4))
-        .replaceAll(/(sex|hoe|wtf|kys|kkk)+/gim, t(3))
+        .replaceAll(/(fuck|dick|cock|cunt|nigg|slut|porn|boob|jizz|milf|dilf|pimp|rape|sext|nude|tits|titt|sexy)+/gim, t(4))
+        .replaceAll(/(hoe|wtf|kys|kkk)+/gim, t(3))
+        .replaceAll(/(69)+/gim, t(2))
         .replaceAll(/(🤱|💦|🕳️|🤤|🍑|🍆|🖕|😩|😫|🍒|🥵|🤓|👙|🥒)+/gim, t(1))
 
         // emojis of dev
